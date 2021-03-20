972c16d3dc9ca88ee63eea72946e85506075401c
Fauzyramadan <fauzyfauzy288@gmail.com>
2021-03-20 16:53:38 +0700

    Tugas 1 Praktikum ALPRO 2021

---
+++ TUGAS 1/TUGAS 1.1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int i,j,hasil;
+	
+	cout << "Nama	: Fauzy Ramadan"<< endl;
+	cout << "NPM	: 20.14.1.0051"<< endl;
+	cout << "Alamat	: Jatiwangi - Majalengka"<< endl;
+	cout << "TTL	: Majalengka, 13 Desember 2001"<< endl;
+	cout << "No. HP	: 089647333794"<< endl;
+	cout << "Motto	: My enemy is me"<< endl;
+	
+	return 0;
+}
---
+++ TUGAS 1/TUGAS 1.2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int i,j,hasil;
+	
+	cout << "Masukan Nilai Ke-1	= ";
+	cin >> i;
+	cout << "Masukan Nilai Ke-2	= ";
+	cin >> j;
+	
+	cout <<endl;
+	
+	hasil = i+j;
+	cout << i << "+" << j << "  	hasilnya " << hasil << endl;
+	hasil = i-j;
+	cout << i << "-" << j << "   	hasilnya " << hasil << endl;
+	hasil = i*j;
+	cout << i << "*" << j << "   	hasilnya " << hasil << endl;
+	hasil = i/j;
+	cout << i << " DIV " << j << "	hasilnya " << hasil << endl;
+	hasil = i%j;
+	cout << i << " MOD " << j << "	hasilnya " << hasil << endl;
+
+	return 0;
+}
